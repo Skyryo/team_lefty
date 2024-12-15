22df3b2f65e13f6f50967bc7ab4cb1906645a643
tochisuke221 <tochisuke221@gmail.com>
2024-12-15 10:41:53 +0900

    すまん

--- app/components/AppWrapper.tsx
+++ app/components/AppWrapper.tsx
@@ -8,7 +8,6 @@ export default function AppWrapper(props: HeaderProps) {
 			className="min-h-screen flex flex-col items-center justify-center p-4"
 			style={{ backgroundColor: "rgb(41, 76, 122)" }} // 背景色を直接指定
 		>
-			<h1 className="text-4xl font-bold mb-8 text-white">商店街の森</h1>
 			{children}
 		</div>
 	);
